9644990417c233a02b86d8ae6d4e44bf42767d6f
BenedictTeaShangZhe <101780516+BenedictTeaShangZhe@users.noreply.github.com>
2022-03-17 15:32:11 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cqqfjwe1w1ma.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = benedictteashangzhe-employee"
+customregion = "us-east-1"
